e8aaf74140654f5018abf00520bc60428fcee50a
Ethical98 <devansh.gupta73@yahoo.in>
2021-06-03 16:11:03 +0530

    Profile Screen Added

---
+++ frontend/src/Screens/ProfileScreen.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import {
+  getUserDetails,
+  updateUserProfile,
+  logout,
+} from '../actions/userActions';
+import jsonwebtoken from 'jsonwebtoken';
+
+const ProfileScreen = ({ history, location }) => {
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
+
+  useEffect(() => {
+    if (userInfo && userInfo.token) {
+      jsonwebtoken.verify(
+        userInfo.token,
+        process.env.REACT_APP_JWT_SECRET,
+        (err, decoded) => {
+          if (err) {
+            dispatch(logout());
+            history.push('/login');
+          } else {
+            if (!user.name) {
+              dispatch(getUserDetails('profile'));
+            } else {
+              setName(user.name);
+              setPhone(user.phone);
+              setEmail(user.email);
+            }
+          }
+        }
+      );
+    }
+  }, [dispatch, userInfo, history, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match');
+    } else {
+      //dispatch update
+      if (phone.toString().length > 10) {
+        const number = Number(phone.toString().split('91')[1]);
+        dispatch(
+          updateUserProfile({ id: user._id, name, email, number, password })
+        );
+      } else {
+        dispatch(
+          updateUserProfile({ id: user._id, name, email, phone, password })
+        );
+      }
+    }
+  };
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {success && <Message variant='success'>Profile Updated</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+
+            <Form.Control
+              type='name'
+              placeholder='Enter Name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId='phone'>
+            <Form.Label>Mobile</Form.Label>
+            <Form.Control
+              type='phone'
+              placeholder='Enter Mobile'
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Confirm Password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type='submit' className='btn-block'>
+            Update
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+    </Row>
+  );
+};
+
+export default ProfileScreen;
