ade0cd13eac54ab8a8d5a72b9348767bc049b064
Ethical98 <devansh.gupta73@yahoo.in>
2021-08-18 17:13:06 +0530

    Deploy to Vercel

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
