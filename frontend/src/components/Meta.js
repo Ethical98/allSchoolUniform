c46f0de27e680e2a3ecdf5df9ce3e3489e5c8ee4
Ethical98 <devansh.gupta73@yahoo.in>
2022-08-08 15:11:08 +0530

    meta tag default added

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -19,7 +19,8 @@ const Meta = ({ title, description, keywords, canonical }) => {
 Meta.defaultProps = {
     title: 'Welcome To AllSchoolUniform',
     description: 'Buy School Uniforms Online',
-    keywords: 'uniform,school,buy,cheap,Asu',
+    keywords:
+        'cheap,sell,buy,allschooluniform,new,buyback,unform,online,GD Goenka Public School,GD,schools,school,presidium,bal bharati public school,dps,kendriya vidyalya,dav,all,smart,air force,uniforms,public',
     canonical: 'https://allschooluniform.com'
 };
 
